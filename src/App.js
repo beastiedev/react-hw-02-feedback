516c56dea83875399d3b228a2c49af1ebe8b2a1d
Pavlo Bessmertnyy <pavlo.bessmertnyy@globallogic.com>
2020-09-07 16:10:23 +0300

    step 1: add state and increment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  onFeedbackAction = (e) => {
+    let state;
+    switch (e) {
+      case 'good':
+        state = { good: this.state.good + 1 }
+        break;
+
+      case 'neutral':
+        state = { neutral: this.state.neutral + 1 }
+        break;
+
+      case 'bad':
+        state = { bad: this.state.bad + 1 }
+        break;
+
+      default:
+        break;
+    }
+    this.setState(state)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>PLease leave feedback</h1>
+
+        <input className="feedback-action" type="button" value="Good" onClick={() => this.onFeedbackAction('good')} />
+        <input className="feedback-action" type="button" value="Neutral" onClick={() => this.onFeedbackAction('neutral')} />
+        <input className="feedback-action" type="button" value="Bad" onClick={() => this.onFeedbackAction('bad')} />
+
+        <h1>Statistics</h1>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
